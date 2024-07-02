fa19431c3f335321303dc4497df1e52bd63d6d72
Vrushali Shinde <60639341+vrushalishinde25@users.noreply.github.com>
2024-07-02 12:06:26 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 	});
 
 	var typed = new Typed(".typed", {
-		strings: ["Software Engineer.", "Web Developer.", "Student."],
+		strings: ["Software Engineer.", "Web Developer.", "Avid Learner."],
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
